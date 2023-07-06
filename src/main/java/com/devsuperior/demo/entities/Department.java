9c32e1e24546fb5c3f0148dad13f6dbc92e4084b
Lucas Sathler <lcssathler@gmail.com>
2023-07-05 22:16:42 -0300

    ORM and H2 Database

--- src/main/java/com/devsuperior/demo/entities/Department.java
+++ src/main/java/com/devsuperior/demo/entities/Department.java
@@ -1,6 +1,16 @@
 package com.devsuperior.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb-department")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
